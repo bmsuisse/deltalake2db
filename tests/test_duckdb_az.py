e1013861cad708d16b25c167346c190c3bf9b518
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2024-04-17 09:35:54 +0200

    support specifing chain

--- tests/test_duckdb_az.py
+++ tests/test_duckdb_az.py
@@ -5,6 +5,43 @@ import polars as pl
 import pytest
 
 
+@pytest.mark.skip(reason="Would need a real account for this")
+def test_chain():
+    from deltalake2db import duckdb_create_view_for_delta
+
+    with duckdb.connect() as con:
+        duckdb_create_view_for_delta(
+            con,
+            "az://testlakedb/td/delta/fake",
+            "delta_table",
+            storage_options={"use_emulator": "True", "chain": "default"},
+        )
+
+        df = pl.from_arrow(con.execute("select * from delta_table").fetch_arrow_table())
+        print(df)
+
+    assert isinstance(df.schema["main_coord"], pl.Struct)
+    fields = df.schema["main_coord"].fields
+    assert "lat" in [f.name for f in fields]
+    assert "lon" in [f.name for f in fields]
+
+    assert isinstance(df.schema["age"], pl.List)
+    assert isinstance(df.schema["age"].inner, pl.Int64)
+    assert df.schema == OrderedDict(
+        [
+            ("Super Name", pl.String),
+            ("Company Very Short", pl.String),
+            ("main_coord", pl.Struct({"lat": pl.Float64, "lon": pl.Float64})),
+            ("coords", pl.List(pl.Struct({"lat": pl.Float64, "lon": pl.Float64}))),
+            ("age", pl.List(pl.Int64)),
+            ("new_name", pl.String),
+        ]
+    )
+
+    as_py_rows = df.rows(named=True)
+    print(as_py_rows)
+
+
 def test_col_mapping(storage_options):
     dt = DeltaTable("az://testlakedb/td/delta/fake", storage_options=storage_options)
 
