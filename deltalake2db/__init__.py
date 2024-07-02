1ae95a19a68c5b6da04acea6f4cd2d75f371476e
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2024-07-02 11:43:02 +0200

    add public get_delta_table method

--- deltalake2db/__init__.py
+++ deltalake2db/__init__.py
@@ -4,3 +4,15 @@ from .duckdb import create_view_for_delta as duckdb_create_view_for_delta
 from .duckdb import apply_storage_options as duckdb_apply_storage_options
 from .polars import scan_delta_union as polars_scan_delta, get_polars_schema
 from .protocol_check import is_protocol_supported
+from .delta_lake import get_delta_table
+
+__all__ = [
+    "get_sql_for_delta",
+    "get_sql_for_delta_expr",
+    "duckdb_create_view_for_delta",
+    "duckdb_apply_storage_options",
+    "polars_scan_delta",
+    "get_polars_schema",
+    "is_protocol_supported",
+    "get_delta_table",
+]
