d52595b9ccb5d193fb2af74fb92ae3c9be773bbc
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2025-06-13 09:10:55 +0200

    fix

--- deltalake2db/arrow_utils.py
+++ deltalake2db/arrow_utils.py
@@ -8,7 +8,6 @@ if TYPE_CHECKING:
 def to_pylist(reader: "pa.RecordBatch|arro3RecordBatch"):
     if hasattr(reader, "to_pylist"):
         return reader.to_pylist()  # type: ignore
-    assert not isinstance(reader, pa.RecordBatch)
     results = [{}] * reader.num_rows
     for i, c in enumerate(reader.column_names):
         array = reader.column(i)
