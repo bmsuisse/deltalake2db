e6767384feeb50222e33d776d7f17a03235f58f5
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2025-09-23 11:36:03 +0200

    fix int partition filter

--- deltalake2db/filter_by_meta.py
+++ deltalake2db/filter_by_meta.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from datetime import date, datetime
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +10,18 @@ def _can_filter(action: dict, conditions: dict):
     try:
         for key, value in conditions.items():
             part_vl = action.get("partitionValues", {}).get(key, None)
+            if isinstance(value, int):
+                part_vl = int(part_vl) if part_vl is not None else None
+            elif isinstance(value, date):
+                part_vl = date.fromisoformat(part_vl) if part_vl is not None else None
+            elif isinstance(value, datetime):
+                part_vl = (
+                    datetime.fromisoformat(part_vl) if part_vl is not None else None
+                )
+            elif isinstance(value, bool):
+                if part_vl is not None:
+                    part_vl = part_vl.lower() in ("true", "1", "t", "y", "yes")
+
             if part_vl is not None and part_vl != value:
                 return True
             stats = {}
@@ -23,12 +36,12 @@ def _can_filter(action: dict, conditions: dict):
             null_count = stats.get("nullCount", {}).get(key, None)
             if null_count == 0 and value is None:
                 return True
-            if isinstance(min_vl, str):
+            if isinstance(min_vl, str) and isinstance(value, str):
                 value = value[0 : len(min_vl)]
             if (
                 min_vl is not None
                 and max_vl is not None
-                and (value < min_vl or value > max_vl)
+                and (value < min_vl or value > max_vl)  # type: ignore
             ):
                 return True
         return False
